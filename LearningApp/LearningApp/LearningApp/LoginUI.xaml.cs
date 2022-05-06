ac4ae1b5e7b04ad1edd4af946c34a0217db570a6
mhsn99 <mahsuntay99@gmail.com>
2022-05-06 14:25:35 +0300

    login page added

---
+++ LearningApp/LearningApp/LearningApp/LoginUI.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LearningApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+        public void LoginClicked(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "FZD" && txtpassword.Text == "0122")
+            {
+                Navigation.PushAsync(new Page1());
+            }
+            else
+            {
+                DisplayAlert("Oops...", "Something is wrong", "OK");
+            }
+        }
+    }
+}
